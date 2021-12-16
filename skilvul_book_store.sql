76637384ef312b365eb1cec2ce56b68ff0c21d1b
IGrizzz <muda3332@gmail.com>
2021-12-17 01:47:58 +0700

    Syaipuddin Revisi

--- skilvul_book_store.sql
+++ skilvul_book_store.sql
@@ -1,57 +1,43 @@
--- Create Database --
-create database skilvulbookstore;
+CREATE DATABASE skilvulbookstore;
 
--- Using The Database --
-use skilvulbookstore;
+USE skilvulbookstore;
 
--- Create Table Penerbit --
-create table penerbit 
-(
-    id int not null auto_increment primary key,
-    nama varchar(50),
-    kota varchar(50)
+CREATE TABLE penerbit(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(50) DEFAULT NULL,
+    kota VARCHAR(50) DEFAULT NULL
 );
 
--- Create Table Penulis --
-create table penulis
-(
-    id int not null auto_increment primary key,
-    nama varchar(50),
-    kota varchar(50)
-);
-
--- Create Table Buku --
-create table buku
-(
-    id int not null auto_increment primary key,
-    ISBN varchar(50),
-    judul varchar(50),
-    penulis varchar(50),
-    penerbit int,
-    harga int,
-    stock int,
-    constraint fk_buku_penulis_id
-    foreign key(id)
-    references penulis(id),
-    constraint fk_buku_penerbit_penerbit
-    foreign key(penerbit)
-    references penerbit(id)
+CREATE TABLE penulis(
+    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(50) NULL,
+    kota VARCHAR(50) NULL );
+    
+    CREATE TABLE buku(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        ISBN VARCHAR(50),
+        judul VARCHAR(50),
+        penulis VARCHAR(50),
+        penerbit INT,
+        harga INT,
+        stock INT,
+        CONSTRAINT fk_buku_penulis_id
+        FOREIGN KEY(id)
+        REFERENCES penulis(id),
+        CONSTRAINT fk_buku_penerbit_penerbit
+        FOREIGN KEY(penerbit)
+        REFERENCES penerbit(id)
 );
 
 -- Select Inner Join --
-select * from buku inner join penerbit on buku.penerbit = penerbit.id;
+SELECT * FROM buku INNER JOIN penerbit ON buku.penerbit = penerbit.id;
 
--- Select Left Join --
-select * from buku left join penerbit on buku.penerbit = penerbit.id;
+SELECT * FROM buku LEFT JOIN penerbit ON buku.penerbit = penerbit.id;
 
--- Select Right Join --
-select * from buku right join penerbit on buku.penerbit = penerbit.id;
+SELECT * FROM buku RIGHT JOIN penerbit ON buku.penerbit = penerbit.id;
 
--- Select MAX --
-select max(harga) from buku where stock < 10;
+SELECT MAX(harga) FROM buku WHERE stock < 10;
 
--- Select MIN --
-select min(harga) from buku;
+SELECT MIN(harga) FROM buku;
 
--- Select Count --
-select count(id) from buku where harga < 100000;
+SELECT COUNT(id) from buku WHERE harga < 100000;
